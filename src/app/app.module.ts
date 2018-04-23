a43d900f9ade483b5b2ebe7c4b671a134a2028a6
NoahOliverRigon <oliverrigonan@gmail.com>
2018-04-23 14:06:59 +0800

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeLandingComponent } from './home/home.landing.component';
 
+const routes: Routes = [
+  { path: '', component: HomeLandingComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-landing',
+  templateUrl: './home.landing.html',
+  styleUrls: ['./home.landing.css']
+})
+export class HomeLandingComponent {
+  title = 'my home';
+}
